97221f5ef3bdcabdbae12f3e865843dce128b546
Vladimir Vlasoff <holdencolfild19@gmail.com>
2020-12-12 19:24:14 +0300

    Выполнен перевод на Kotlin

--- app/src/main/java/com/example/kotlinhomework/MainActivity.kt
+++ app/src/main/java/com/example/kotlinhomework/MainActivity.kt
@@ -1,69 +1,57 @@
-package com.example.kotlinhomework;
+package com.example.kotlinhomework
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import android.os.Bundle
+import com.example.kotlinhomework.R
+import com.example.kotlinhomework.MainActivity
+import android.content.Intent
+import com.example.kotlinhomework.SecondActivity
+import android.app.Activity
+import android.widget.Button
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
-
-    private String greetings;
-    private String name;
-    private TextView textView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        greetings = getString(R.string.hello);
-        if (savedInstanceState != null && savedInstanceState.getString(NAME_KEY) != null) {
-            name = savedInstanceState.getString(NAME_KEY);
+class MainActivity : AppCompatActivity() {
+    private var greetings: String? = null
+    private var name: String? = null
+    private var textView: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        greetings = getString(R.string.hello)
+        name = if (savedInstanceState?.getString(NAME_KEY) != null) {
+            savedInstanceState.getString(NAME_KEY)
         } else {
-            name = getString(R.string.anon);
+            getString(R.string.anon)
         }
-
-        textView = findViewById(R.id.textViewHello);
-
-        Button button = findViewById(R.id.buttonNameYourSelf);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivityForResult(intent, SecondActivity.GET_NAME_REQUEST_CODE);
-            }
-        });
+        textView = findViewById(R.id.textViewHello)
+        val button = findViewById<Button>(R.id.buttonNameYourSelf)
+        button.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivityForResult(intent, SecondActivity.GET_NAME_REQUEST_CODE)
+        }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == SecondActivity.GET_NAME_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
-            String nameFromData = data.getStringExtra(SecondActivity.NAME_KEY);
-
+            val nameFromData = data.getStringExtra(SecondActivity.NAME_KEY)
             if (nameFromData != null) {
-                name = nameFromData;
+                name = nameFromData
             }
         }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        textView.setText(String.format("%s, %s!", greetings, name));
+    override fun onResume() {
+        super.onResume()
+        textView!!.text = String.format("%s, %s!", greetings, name)
     }
 
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(NAME_KEY, name);
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(NAME_KEY, name)
     }
 
-    private static final String NAME_KEY = "com.example.kotlinhomework.MainActivity.NAME_KEY";
+    companion object {
+        private const val NAME_KEY = "com.example.kotlinhomework.MainActivity.NAME_KEY"
+    }
 }
--- app/src/main/java/com/example/kotlinhomework/SecondActivity.kt
+++ app/src/main/java/com/example/kotlinhomework/SecondActivity.kt
@@ -1,57 +1,50 @@
-package com.example.kotlinhomework;
+package com.example.kotlinhomework
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
-import androidx.appcompat.app.AppCompatActivity;
+class SecondActivity : AppCompatActivity() {
 
-public class SecondActivity extends AppCompatActivity {
+    private lateinit var editText: EditText
+    private lateinit var button: Button
 
-    private EditText editText;
-    private Button button;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
-
-        editText = findViewById(R.id.editTextTextPersonName);
-        button = findViewById(R.id.buttonName);
-        button.setEnabled(false);
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        editText = findViewById(R.id.editTextTextPersonName)
+        button = findViewById(R.id.buttonName)
+        button.isEnabled = false
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
                 // Nothing to do
             }
 
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
                 // Nothing to do
             }
 
-            @Override
-            public void afterTextChanged(Editable editable) {
-                button.setEnabled(!TextUtils.isEmpty(editable));
+            override fun afterTextChanged(editable: Editable) {
+                button.isEnabled = !TextUtils.isEmpty(editable)
             }
-
-        });
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.putExtra(NAME_KEY, editText.getText().toString());
-                setResult(RESULT_OK, intent);
-                finish();
-            }
-        });
+        })
+        button.setOnClickListener(View.OnClickListener {
+            val intent = Intent()
+            intent.putExtra(NAME_KEY, editText.text.toString())
+            setResult(RESULT_OK, intent)
+            finish()
+        })
     }
 
-    public static final String NAME_KEY = "com.example.kotlinhomework.SecondActivity.NAME_KEY";
-    public static final int GET_NAME_REQUEST_CODE = 1234;
+    companion object {
+        const val NAME_KEY = "com.example.kotlinhomework.SecondActivity.NAME_KEY"
+        const val GET_NAME_REQUEST_CODE = 1234
+    }
 }
